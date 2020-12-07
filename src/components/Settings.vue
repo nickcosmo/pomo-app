3a29d5defee0d3506caa399ba286719b883fcddd
Nick <30679225+nickcosmo@users.noreply.github.com>
2020-12-07 22:32:15 +0100

    add settings page

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,31 +1,74 @@
 <template>
-  <h1>settings</h1>
-  <div>
-    <label>study length</label>
-    <slider></slider>
-  </div>
-  <div>
-    <label>break length</label>
-    <slider></slider>
-  </div>
-  <div>
-    <label>long break length</label>
-    <slider></slider>
+  <div class="container">
+    <h1>settings</h1>
+    <div class="slider-container">
+      <label>study length: {{ studyLength }}</label>
+      <slider @slide="update" :val="studyLength" length="study"></slider>
+    </div>
+    <div class="slider-container">
+      <label>break length: {{ breakLength }}</label>
+      <slider @slide="update" :val="breakLength" length="break"></slider>
+    </div>
+    <div class="slider-container">
+      <label>long break length: {{ longBreakLength }}</label>
+      <slider @slide="update" :val="longBreakLength" length="long-break"></slider>
+    </div>
+    <base-button>update profile</base-button>
   </div>
 </template>
 
 <script>
 import Slider from "./UI/Slider.vue";
+import BaseButton from "./UI/BaseButton.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      studyLength: 5,
+      breakLength: 5,
+      longBreakLength: 5,
+    };
+  },
+  methods: {
+    update(value) {
+      if (value[1] === "study") {
+        this.studyLength = value[0];
+      }
+      else if (value[1] === "break") {
+        this.breakLength = value[0];
+      } else {
+        this.longBreakLength = value[0];
+      }
+    },
   },
   components: {
     Slider,
+    BaseButton
   },
 };
 </script>
 
 <style scoped>
+h1 {
+  font-size: 60px;
+}
+
+label {
+  font-size: 30px;
+}
+
+.container {
+  width: 40%;
+  margin: auto;
+  border: 2px solid #efefef;
+  padding: 10px;
+}
+
+.slider-container {
+  margin-top: 10px;
+}
+
+button {
+  margin-top: 10px;
+}
 </style>
--- src/components/UI/Slider.vue
+++ src/components/UI/Slider.vue
@@ -7,34 +7,49 @@
       class="slider"
       id="myRange"
       v-model="slide"
+      @change="update()"
     />
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            slide: 5
-        }
-    }
+  props: ["val", "length"],
+  data() {
+    return {
+      slide: this.val,
+      
+    };
+  },
+  methods: {
+    update() {
+      this.$emit("slide", [this.slide, this.length]);
+    },
+  },
+  emits: ["slide"],
 };
 </script>
 
 <style scoped>
+div {
+  margin-top: 10px;
+}
+
 .slider {
-    -webkit-appearance: none;
-    appearance: none;
-    background: #efefef;
-    border-radius: 20px;
-    outline: none;
+  -webkit-appearance: none;
+  appearance: none;
+  width: 60%;
+  height: 30px;
+  background: #efefef;
+  border-radius: 20px;
+  outline: none;
 }
 
 .slider::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
-  width: 15px;
-  height: 15px;
+  width: 30px;
+  height: 30px;
   border-radius: 50%;
   background: #9999;
   cursor: pointer;
